1406d57f76cecd11cf962a1a10bbd4532dffbb26
joyoon <joyoon729@gmail.com>
2019-05-28 01:43:59 +0900

    190528 | add file_read_write.cpp

---
+++ file_read_write.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+	ifstream read;
+	ofstream write;
+	read.open("sample.txt");
+	write.open("write_sample.txt");
+	char jump = NULL;
+
+	// sample.txt 파일 길이만큼 문자형 배열 만든다
+	char str[sizeof(read)]={'\0'};
+
+	int i=0;
+
+	if(read.good()){ // 텍스트 파일이 잘 열리면 실행
+		while(!read.eof()){ // 텍스트 파일에 더이상 읽을게 없으면 종료
+			read.getline(str,sizeof(read)); // getline. 불러온 텍스트 파일을 문자 하나씩, 텍스트 파일 크기만큼 읽어들인다.
+			for(i=0; i<sizeof(read); i++){ // 문자 하나씩 읽어온다.
+				write << str[i];				
+			}
+			if(jump != '\n') write << endl;
+			fill_n(str,100,'\0');
+		} 
+	}
+	read.close();
+	write.close();
+
+	return 0;
+}
