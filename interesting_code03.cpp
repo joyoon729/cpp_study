d185a44d942b801f9dcfa5014b2a6dc4bedc93a9
joyoon <joyoon729@gmail.com>
2019-06-19 08:49:13 +0900

    190619 | add interesting_code03.cpp

--- interesting_code03.cpp
+++ interesting_code03.cpp
@@ -4,17 +4,27 @@
 ----------------------------------------------------*/
 #include <iostream>
 #include <climits>
-#include <cstdlib>
 using namespace std;
 
-unsigned abs(int x){
+unsigned my_iabs(int x){
 	int const m = x >> sizeof(int) * CHAR_BIT -1;
 	return (x ^ m) -m;
 }
 
+
+float my_fabs(float p)
+{
+      (int&)p &= 0x7FFFFFFF;
+      return p;
+}
+
+unsigned abss(int z){
+		return unsigned(z);
+}
 int main(){
 	int x = -3;
-
-	cout << "x = " << x << endl;
-	cout << "abs(x) = " << abs(x) << endl;
+	float y = -3;
+	printf("x = %d, my_iabs(x) = %d\n", x, my_iabs(x));
+	printf("y = %f, my_fabs(y) = %f\n", y, my_fabs(y));
+	printf("x = %d, abss(x) = %d\n", x, abss(x));
 }
